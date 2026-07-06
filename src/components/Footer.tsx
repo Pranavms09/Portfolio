d0bb2884c59294e847e688fab7118dd36f473238
pranavms09 <pranavms09@gmail.com>
2026-07-06 23:10:58 +0530

    footer modified

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,32 +9,6 @@ export default function Footer() {
         <div className="text-label-bold font-black text-primary uppercase text-center md:text-left">
           © {currentYear} PRANAV SURYAWANSHI. ALL RIGHTS RESERVED.
         </div>
-        <div className="flex gap-12 text-label-bold uppercase">
-          <a
-            className="text-on-surface-variant hover:text-primary underline transition-all duration-200"
-            href="https://github.com/Pranavms09"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-          <a
-            className="text-on-surface-variant hover:text-primary underline transition-all duration-200"
-            href="https://linkedin.com/in/pranavms09"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-          <a
-            className="text-on-surface-variant hover:text-primary underline transition-all duration-200"
-            href="https://www.instagram.com/pranavms_09/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Instagram
-          </a>
-        </div>
       </div>
     </footer>
   );
